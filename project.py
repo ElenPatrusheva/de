9055a728d904fc4124a016b8f7745c7e5da48b4e
elena <helen.patrusheva@mail.ru>
2018-10-26 22:02:59 +0300

    runge

--- project.py
+++ project.py
@@ -1,42 +1,49 @@
-
 import matplotlib.pyplot as plot
+
 def f(x, y):
     return x**3*y**4-y/x
-def f_improved(x,y,h):
-    return y+h/2*(f(x,y)+f(x+h, y+h*f(x,y)))
+    
 def f_euler(x, y, h):
     return y+h*f(x,y)
-  
-n=10
-x0=1
-y0=0.5
-x=5
-h=(x-x0)/n
-x_v=[x0]
-y_euler=[y0]
-y_original=[y0]
-y_improved=[y0]
-e=[0]
 
-i=x0
-while (i<x):
-    y_original.append((11-3*x_v[-1])**(-1/3))
-    y_euler.append(f_euler(y_v[-1],x_v[-1], h))
-    y_improved.append(f_improved(x_v[-1], y_improved[-1], h))
-    x_v.append(i)
-    e.append(y_v[-1]-y_original[-1])
-    i+=h
-    print(i)
-    
-plot.plot(x_v, y_euler)
-plot.show()
+def f_improved(x, y, h):
+    return y+h/2*(f(x,y)+f(x+h, y+h*f(x,y)))
 
-plot.plot(x_v, y_original)
-plot.show()
+def f_runge(x, y, h):
+    k1 = f(x, y)
+    k2 = f(x + h/2, y + h/2 * k1)
+    k3 = f(x + h/2, y + h/2 * k2)
+    k4 = f(x + h, y + h * k3)
+    return y + h/6 * (k1 + 2*k2 +2*k3 + k4)
 
-plot.plot(x_v, e)
-plot.show()
+main(){
+	x=[x0]
+	y_euler=[y0]
+	y_original=[y0]
+	y_improved=[y0]
+	y_runge=[y0]
+	e=[0]
+	i=x0
 
-plot.plot(x_v, y_improved)
-plot.show()
+	while (i<xf):
+    	y_original.append(x[-1]**(-1)*(11-3*x[-1])**(-1/3))
+    	y_euler.append(f_euler(x[-1], y_euler[-1], h))
+    	y_improved.append(f_improved(x[-1], y_improved[-1], h))
+    	y_runge.append(f_runge(x[-1], y_runge[-1], h))
+    	x.append(i)
+    	#e.append(y_euler[-1]-y_improved[-1])
+    	i+=h
+    	print(i)
 
+    plot.plot(x, y_original)
+	plot.show()
+
+	plot.plot(x, y_euler)
+	plot.show()
+
+	plot.plot(x, y_improved)
+	plot.show()
+
+	plot.plot(x, y_runge)
+	plot.show()
+}
